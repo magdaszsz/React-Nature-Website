e455c9fc205cd8ced741be4ad881b6c0c34e5551
Magda Sz <pesahson@gmail.com>
2021-07-31 18:02:30 +0200

    menu for mobile done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,37 @@
 import React, {useState} from 'react';
 import { WiFire } from "react-icons/wi";
 import {NavLink} from 'react-router-dom';
+import { GoThreeBars } from "react-icons/go";
+import { GoX } from "react-icons/go";
 import './Navbar.css';
 
 function Navbar() {
+  const [click, setClick] = useState(false)
   return (
-   <>
-   <nav className="navbar">
-   
-      <div>
-      <NavLink 
-      exact to="/"><WiFire class="logo"/></NavLink>
-      </div>
-      <ul>
-        <li>
-          <NavLink exact to="/mountains">
-          Mountains
-          </NavLink>
-        </li>
-        <li>
-          <NavLink exact to="/water">
-          Water
-          </NavLink>
-        </li>
-      </ul>
-   
-   </nav>
-   </>
-  )
+    <>
+      <nav>
+        <NavLink exact to="/">
+          <WiFire class="logo" onClick={() => setClick(false)} />
+        </NavLink>
+
+        <div onClick={() => setClick(!click)} className="menu-icon">
+          {click ? <GoX /> : <GoThreeBars />}
+        </div>
+        <ul className={click ? "nav-menu show" : "nav-menu"}>
+          <li>
+            <NavLink exact to="/mountains" onClick={() => setClick(false)}>
+              Mountains
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/water" onClick={() => setClick(false)}>
+              Water
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar
